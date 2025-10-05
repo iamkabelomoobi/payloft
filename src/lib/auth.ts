85e793eb4eb390eaa61df28c8f963cc98afd01ee
iamkabelomoobi <giftmoobi@gmail.com>
2025-10-05 18:32:26 +0200

    ✨ feat: implement authentication client with email and social provider support

---
+++ src/lib/auth.ts
@@ -0,0 +1,34 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { nextCookies } from "better-auth/next-js";
+import { Resend } from "resend";
+import ForgotPasswordEmail from "./templates/auth/ForgotPasswordEmail";
+
+const resend = new Resend(process.env.RESEND_API_KEY as string);
+
+export const authClient = betterAuth({
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
+  emailAndPassword: {
+    enabled: true,
+    sendResetPassword: async ({ user, url }) => {
+      resend.emails.send({
+        from: "No Reply <kabelom@bbbsoftware.co.za>",
+        to: user.email,
+        subject: "Reset your password",
+        react: ForgotPasswordEmail({ email: user.email, url }),
+      });
+    },
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    },
+  },
+  plugins: [nextCookies()],
+});
